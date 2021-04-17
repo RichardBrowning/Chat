74c6cc00d1f2f654fe43c1ccaa3e7560d68f0dba
SChen <cchhtt1999@163.com>
2021-04-17 16:46:39 +0800

    exit upon exception

---
+++ Chat0.9/src/ChatServer.java
@@ -0,0 +1,43 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.*;
+
+public class ChatServer {
+
+    public static void main(String[] args){
+        boolean started = false;
+        ServerSocket ss = null;
+        Socket s = null;
+        DataInputStream DIS = null;
+        try {
+            ss = new ServerSocket(8888);
+            started = true;
+            while(started){
+                s = ss.accept();
+                boolean bConnected = false;
+System.out.println("a client connected.");//debug line
+                bConnected = true;
+                DIS = new DataInputStream(s.getInputStream());
+                while(bConnected){
+                    String str = DIS.readUTF();
+                    System.out.println(str);
+                }
+                //DIS.close();
+            }
+        } catch (Exception e) {
+            System.out.println("client closed");
+
+            //e.printStackTrace();
+
+        } finally {
+            try{
+                if (DIS != null) DIS.close();
+                if (s != null) s.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+    }
+}
